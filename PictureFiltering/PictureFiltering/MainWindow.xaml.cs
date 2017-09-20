41fb74dac36b0c43a27462209d75cc701619a83d
Christopher WUTTI <wuttic@edu.htl-villach.at>
2017-09-20 19:43:41 +0200

    GUI + Picking picture

--- PictureFiltering/PictureFiltering/MainWindow.xaml.cs
+++ PictureFiltering/PictureFiltering/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,47 @@ namespace PictureFiltering
         public MainWindow()
         {
             InitializeComponent();
+
+            initOtherComponents();
+        }
+
+        private void initOtherComponents()
+        {
+            setBackgroundColor();
+        }
+
+        private void setBackgroundColor()
+        {
+            BrushConverter bc = new BrushConverter();
+            Brush brush = (Brush)bc.ConvertFrom("#012D41");
+            brush.Freeze();
+            mainWindow.Background = brush;
+        }
+
+        private void Open_Picture(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("Opening Picture!");
+            //Stream picStream = null;
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            dialog.Filter = "Image Files(*.bmp;*.jpg;)| *.BMP;*.JPG;";
+
+            if (dialog.ShowDialog() == true)
+            {
+                Console.WriteLine("wohoo: " + dialog.FileName);
+
+                BitmapImage pic = new BitmapImage();
+                pic.BeginInit();
+                pic.UriSource = new Uri(dialog.FileName);
+                pic.EndInit();
+
+                SourcePic.Source = pic;
+            }
+        }
+        
+        private void Save_Picture(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
